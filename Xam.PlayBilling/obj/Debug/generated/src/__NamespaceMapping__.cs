7bf11632e80f6cf07b7f509ee4c04d5908dd59a1
Shibly <shibly77@outlook.com>
2019-08-04 16:14:35 +0600

    git ignore update

--- Xam.PlayBilling/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.android.billingclient", Managed="Android.BillingClient")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.android.billingclient.api", Managed="Android.BillingClient.Api")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.android.billingclient.util", Managed="Android.BillingClient.Util")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.android.vending.billing", Managed="Android.Vending.Billing")]
